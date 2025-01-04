724efa292b9d2ec76c0fc85b243400b78dc601f2
Harding10 <behdegryjeremieharding@gmail.com>
2025-01-04 17:30:44 +0000

    Add files via upload

---
+++ apprandre-js/scripts/main.js
@@ -0,0 +1,15 @@
+lancerJeu()
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let divZoneProposition = document.querySelector(".zoneProposition")
+console.log(divZoneProposition)
+
+let spanScore = document.querySelector (".zoneScore span")
+console.log(spanScore)
+
+let listeRadio = document.querySelectorAll (".zoneOptions input")
+console.log(listeRadio)
